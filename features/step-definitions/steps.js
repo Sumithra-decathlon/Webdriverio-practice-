3a225340ce8b245ff0b5a2b0c3391971c273a664
SumithraVA <120664621+SumithraVA@users.noreply.github.com>
2023-01-26 00:57:17 +0530

    Update steps.js

--- features/step-definitions/steps.js
+++ features/step-definitions/steps.js
@@ -1,22 +1,19 @@
 const { Given, When, Then } = require('@wdio/cucumber-framework');
 
-const LoginPage = require('../pageobjects/login.page');
-const SecurePage = require('../pageobjects/secure.page');
+const LoginPage = require('../pageobjects/Register');
 
-const pages = {
-    login: LoginPage
-}
 
-Given(/^I am on the (\w+) page$/, async (page) => {
-    await pages[page].open()
+
+Given(/^I am on the home page$/, async (page) => {
+     await LoginPage.openWeburl;
 });
 
-When(/^I login with (\w+) and (.+)$/, async (username, password) => {
-    await LoginPage.login(username, password)
+When(/^I login with (.+),(.+), (.+), (.+),(.+)and (.+)$/, async (irstname, lastname, email, telephone,password,confirmpassword) => {
+    await LoginPage.login(irstname, lastname, email, telephone,password,confirmpassword)
 });
 
-Then(/^I should see a flash message saying (.*)$/, async (message) => {
-    await expect(SecurePage.flashAlert).toBeExisting();
-    await expect(SecurePage.flashAlert).toHaveTextContaining(message);
+Then(/^I should see a flash message saying (.+)$/, async (message) => {
+    expect(LoginPage.getmessage).toBeExisting();
+    expect(LoginPage.getmessage).toHaveTextContaining(message);
 });
 
